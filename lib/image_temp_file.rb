08efa5d8afb3df4b31e2d0da3ad3635ca2dd8d73
Cannikin <cannikinn@gmail.com>
2010-08-30 12:44:15 -0700

    Fixed issue with creating temp file names

--- lib/image_temp_file.rb
+++ lib/image_temp_file.rb
@@ -2,8 +2,11 @@ require "tempfile"
 
 module MiniMagick
   class ImageTempFile < Tempfile
-    def make_tmpname(ext)
-      'mini_magick%d-%d%s' % [$$, Time.now.to_f, ext ? ".#{ext}" : '']
+    def make_tmpname(ext, n)
+      puts "ext: #{ext.to_s}"
+      puts "n: #{n.to_s}"
+      n = Time.now.to_f if n.nil?
+      'mini_magick%s-%s%s' % [$$.to_s, n.to_s, ext ? ".#{ext}" : '']
     end
   end
 end
